e6396ac1994114360c68348f3c054c815d306ef9
Sebastian Home <sebastianmoldrup@Sebastians-Air.home>
2025-02-26 00:10:21 +0100

    added tailwindcss support

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,13 @@ return {
   "folke/snacks.nvim",
   opts = {
     explorer = { enabled = false },
+    indent = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    scope = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false }, -- we set this in options.lua
+    toggle = { map = LazyVim.safe_keymap_set },
+    words = { enabled = false },
   },
 }
